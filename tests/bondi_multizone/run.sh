62e45cd90afa8292569182095eb602f293d004b6
Hyerin Cho <chyerin1996@gmail.com>
2023-03-21 10:03:35 -0400

    updated the test batch scripts

--- tests/bondi_multizone/run.sh
+++ tests/bondi_multizone/run.sh
@@ -1,31 +1,31 @@
 #!/bin/bash 
 # Hyerin (02/17/23) copied from Cora's code
 
-# Bash script testing b_clean
+# Bash script testing HD bondi
 
 # User specified values here
 KERR=false
-bz=5e-3
+JITTER=false #true #
 DIM=3
-NZONES=2 #7
+NZONES=7
 BASE=8
-NRUNS=100
-START_RUN=0 # if this is not 0, then update start_time, out_to_in, iteration, r_out, r_in to values that you are re-starting from
-DRTAG="bondi_multizone_031123_bclean_${bz}_flr_test"
+NRUNS=300
+START_RUN=53 # if this is not 0, then update start_time, out_to_in, iteration, r_out, r_in to values that you are re-starting from
+DRTAG="bondi_multizone_030723_bondi_128^3"
 
 # Set paths
+KHARMADIR=../..
 PDR="/n/holylfs05/LABS/bhi/Users/hyerincho/grmhd/" ## parent directory
 DR="${PDR}data/${DRTAG}"
 parfilename="${PDR}/kharma/pars/bondi_multizone/bondi_multizone_00000.par" # parameter file
-#parfilename="${PDR}/sane_save.par" # parameter file
 
 # other values determined automatically
 turn_around=$(($NZONES-1))
-start_time=0
-out_to_in=1
-iteration=1
-r_out=$((${BASE}**($turn_around+2)))
-r_in=$((${BASE}**$turn_around))
+start_time=32963095169 #0 #
+out_to_in=1 # -1 #
+iteration=9 # eq : (iteration-1)*(NZONES-1)<VAR<=iteration*(NZONES-1)
+r_out=512 #$((${BASE}**($turn_around+2))) #
+r_in=8 #$((${BASE}**$turn_around)) #
 
 # if the directories are not present, make them.
 if [ ! -d "${DR}" ]; then
@@ -39,7 +39,7 @@ fi
 for (( VAR=$START_RUN; VAR<$NRUNS; VAR++ ))
 do
   args=()
-  echo "${DRTAG}: iter $iteration, $VAR : t = $start_time, r_out = $r_out, r_in = $r_in"
+  echo "${DRTAG} iter $iteration, $VAR : t = $start_time, r_out = $r_out, r_in = $r_in"
   logruntime=`echo "scale=20; l($r_out)*3./2-l(1.+$r_out/100000)/2." | bc -l` # round to an integer for the free-fall time (cs^2=0.01 should be updated from the desired rs value) # GIZMO
   runtime=`echo "scale=0; e($logruntime)+1" | bc -l`
   log_u_over_rho=-5.2915149 # test same vacuum conditions as r_shell when (rs=1e2.5)
@@ -49,11 +49,9 @@ do
   
   # set problem type and cleanup
   if [ $VAR -eq 0 ]; then
-    prob="bondi" #"torus" #
-    init_c=0
+    prob="bondi"
   else
     prob="resize_restart_kharma"
-    init_c=1
   fi
   
   # set BH spin
@@ -65,8 +63,8 @@ do
   
   # output time steps
   output0_dt=$((${runtime}/100*10))
-  #output1_dt=$((${runtime}/20*10))
-  output1_dt=$((${runtime}/50*10))
+  output1_dt=$((${runtime}/20*10))
+  #output1_dt=$((${runtime}/200*10)) # test Hyerin (02/20/23)
   output2_dt=$((${runtime}/1000*10))
   
   # dt, fname, fname_fill
@@ -89,12 +87,19 @@ do
     else
       fname_fill="none"
     fi
-    args+=(" resize_restart/fname=$fname parthenon/time/dt_min=$dt_new")
+    args+=(" resize_restart/fname=$fname resize_restart/use_dt=false parthenon/time/dt_min=$dt_new")
     args+=(" resize_restart/fname_fill=$fname_fill ")
   else
     r_shell=$((${r_out}/2))
     args+=(" bondi/r_shell=$r_shell ")
+    if [[ $JITTER == "true" ]]; then
+        args+=(" perturbation/u_jitter=0.3 ")
+    else
+        args+=(" perturbation/u_jitter=0.0 ")
+    fi
   fi
+
+  
 
   # data_dir, logfiles
   data_dir="${DR}/bondi_multizone_$(printf %05d ${VAR})"
@@ -102,27 +107,22 @@ do
   err_fn="${PDR}/logs/${DRTAG}/log_multizone$(printf %05d ${VAR})_err"
 
   srun --mpi=pmix ${PDR}/kharma.cuda -i ${parfilename} \
-                                    parthenon/mesh/nx1=64 parthenon/mesh/nx2=64 parthenon/mesh/nx3=64 \
-                                    parthenon/meshblock/nx1=16 parthenon/meshblock/nx2=64 parthenon/meshblock/nx3=64 \
                                     parthenon/job/problem_id=$prob \
-                                    parthenon/time/tlim=${start_time} \
-                                    coordinates/r_in=${r_in} coordinates/r_out=${r_out}  coordinates/a=$spin coordinates/hslope=1 coordinates/transform=mks \
-                                    bondi/vacuum_logrho=-8.2014518 bondi/vacuum_log_u_over_rho=${log_u_over_rho} \
-                                    floors/disable_floors=false floors/rho_min_geom=1e-6 floors/u_min_geom=1e-8 \
-                                    floors/bsq_over_rho_max=100 floors/bsq_over_u_max=50 \
-                                    b_field/type=vertical b_field/solver=flux_ct b_field/bz=${bz} \
-                                    b_field/fix_flux_x1=0 b_field/initial_cleanup=$init_c \
-                                    b_cleanup/rel_tolerance=1.e-8 \
+                                    parthenon/time/tlim=${start_time} parthenon/time/nlim=-1 \
+                                    parthenon/mesh/nx1=128 parthenon/mesh/nx2=128 parthenon/mesh/nx3=128 \
+                                    parthenon/meshblock/nx1=64 parthenon/meshblock/nx2=64 parthenon/meshblock/nx3=128 \
+                                    coordinates/r_in=${r_in} coordinates/r_out=${r_out} coordinates/a=$spin coordinates/ext_g=false \
+                                    coordinates/transform=mks coordinates/hslope=1 \
+                                    bounds/fix_flux_pole=1 \
+                                    bondi/vacuum_logrho=-8.2014518 bondi/vacuum_log_u_over_rho=${log_u_over_rho} bondi/use_gizmo=false \
+                                    b_field/type=none b_field/solver=none b_field/bz=1e-3 \
+                                    b_field/fix_flux_x1=0 b_field/initial_cleanup=0 \
                                     resize_restart/base=$BASE resize_restart/nzone=$NZONES resize_restart/iteration=$iteration\
                                     parthenon/output0/dt=$output0_dt \
                                     parthenon/output1/dt=$output1_dt \
                                     parthenon/output2/dt=$output2_dt \
                                     ${args[@]} \
                                     -d ${data_dir} 1> ${out_fn} 2>${err_fn}
-                                    # nlim=10000 for 1e-3   
-                                    # floors/u_over_rho_max=2 
-                                    #b_field/fix_flux_x1=1 b_field/initial_cleanup=0 \
-                                    #coordinates/transform=mks coordinates/hslope=1 \ this, for some reason does not work for b cleaning?
 
   if [ $VAR -ne 0 ]; then
     if [ $(($VAR % ($NZONES-1))) -eq 0 ]; then
